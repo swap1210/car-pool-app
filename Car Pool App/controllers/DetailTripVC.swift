817076c3039b6ab46943faa85987a2d5dc56affa
Swapnil Patel <p.swap1210@gmail.com>
2022-11-14 18:16:41 -0600

    added VC to the DetailTrip scene

---
+++ Car Pool App/controllers/DetailTripVC.swift
@@ -0,0 +1,31 @@
+//
+//  DetailTripVC.swift
+//  Car Pool App
+//
+//  Created by Swapnil Patel on 11/14/22.
+//
+
+import UIKit
+
+class DetailTripVC: UIViewController {
+    var isDriver:Bool!
+    var TripId:Int!
+    var trip:Trip!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
