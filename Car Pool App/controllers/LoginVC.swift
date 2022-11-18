7797ebf5002ef16d15582ff927eac2cda5df6d67
Swapnil Patel <p.swap1210@gmail.com>
2022-11-17 23:17:12 -0600

    login and new user listeners stopped on view disappear

--- Car Pool App/controllers/LoginVC.swift
+++ Car Pool App/controllers/LoginVC.swift
@@ -10,25 +10,34 @@ import FirebaseAuth
 
 class LoginVC: UIViewController {
     
+    var userListener:AuthStateDidChangeListenerHandle?
     @IBOutlet weak var passwordTF: UITextField!
     @IBOutlet weak var userNameTF: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         do{
+            print("Trying to log out")
             try Auth.auth().signOut()
-        }catch err: {
-            
+            print("logged out")
+        }catch let err {
+            print("Error logging out ",err)
         }
     }
     
     override func viewDidAppear(_ animated: Bool) {
         self.navigationItem.title = "Login"
-        _ = Auth.auth().addStateDidChangeListener { auth, user in
+        self.userListener = Auth.auth().addStateDidChangeListener { auth, user in
             if(user != nil){
                 print("login user!",user!)
                 self.goToLogin()
             }
         }
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+            if let handle = self.userListener{
+                Auth.auth().removeStateDidChangeListener(handle)
+            }
     }
     
     
@@ -39,7 +48,7 @@ class LoginVC: UIViewController {
         if let username = userNameTF.text{
             if let psd = passwordTF.text{
                 Auth.auth().signIn(withEmail: username, password: psd) { authResult, error in
-                    print("Auth results ",error!)
+                    print("Auth results ",error)
                 }
             }
         }
