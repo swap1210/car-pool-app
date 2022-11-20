b20b6166f1bcd37064fa009b1d390604d1a8a639
BaoTLy <116236843+BaoTLy@users.noreply.github.com>
2022-11-19 18:09:15 -0600

    Manually Update Ride

--- Car Pool App/model/Ride.swift
+++ Car Pool App/model/Ride.swift
@@ -13,8 +13,9 @@ struct Ride{
     var to: String
     var timeFrom: Timestamp
     var timeTo: Timestamp
-    var driver: String?
+    var driver: String
     var passengers: [String]
+    var tripID: Int?
     
     init(dictionary: NSDictionary){
         self.from = (dictionary["from"] as? String ?? "")
@@ -22,17 +23,19 @@ struct Ride{
         self.timeTo = (dictionary["timeTo"] as! Timestamp)
         self.timeFrom = (dictionary["timeFrom"] as! Timestamp)
         self.passengers = (dictionary["passengers"] as? [String] ?? [])
+        self.driver = (dictionary["driver"] as? String ?? "")
     }
     
-    init(from: String,to: String,timeFrom: Timestamp,timeTo: Timestamp,passengers: [String] = []){
+    init(from: String,to: String,timeFrom: Timestamp,timeTo: Timestamp,driver: String,passengers: [String] = []){
         self.from = from
         self.to = to
         self.timeFrom = timeFrom
         self.timeTo = timeTo
         self.passengers = passengers
+        self.driver = driver
     }
     
     func toNSDictionary()-> NSDictionary{
-        return ["from":self.from,"to":self.to,"timeFrom":self.timeFrom,"timeTo":self.timeTo,"passengers":self.passengers];
+        return ["from":self.from,"to":self.to,"timeFrom":self.timeFrom,"timeTo":self.timeTo,"passengers":self.passengers,"driver":self.driver];
     }
 }
