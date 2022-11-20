3a741fe6cec517e75ba0a34a416fb71b5f93bb3d
Swapnil Patel <p.swap1210@gmail.com>
2022-11-20 02:00:16 -0600

    Error added in add trip screen Chatroom gets created when new trip is created

--- Car Pool App/controllers/AddTripVC.swift
+++ Car Pool App/controllers/AddTripVC.swift
@@ -73,26 +73,66 @@ class AddTripVC: UIViewController {
             passengersArr.append(curPass)
         }
         
-        if (from != nil && to != nil && fromAtVal < toAtVal){
+        if (from != nil && from != "" && to != nil && to != "" && fromAtVal < toAtVal){
             self.addRide(ride: Ride(from: from!, to: to!, timeFrom: Timestamp(date:fromAtVal), timeTo: Timestamp(date:toAtVal),driver: "", passengers: passengersArr))
+        }else{
+            var message:String = ""
+            
+            if (from != nil || from != ""){
+                message = "From address is invalid. "
+            }
+            
+            if (to != nil && to != ""){
+                message = message + "To address is invalid. "
+            }
+            
+            if (fromAtVal < toAtVal){
+                message = message + "from date must be before to date. "
+            }
+            
+            if (fromAtVal < Date()){
+                message = message + "from date can't be a past date."
+            }
+            
+            let alertController = UIAlertController(title: "Input Error", message: message, preferredStyle: .alert)
+                    
+                    let OKAction = UIAlertAction(title: "OK", style: .default) { (action:UIAlertAction!) in
+                        
+                        // Code in this block will trigger when OK button tapped.
+                        print("Ok button tapped");
+                        
+                    }
+                    
+                    alertController.addAction(OKAction)
+                    
+                    self.present(alertController, animated: true, completion:nil)
         }
     }
     
     func addRide(ride: Ride){
         let ridesRef = db.collection(Common.CPcollection).document(Common.document)
-        ridesRef.updateData([Common.mainField+".records."+String(self.currentCount+1):ride.toNSDictionary()]){err in
+        let countval = self.currentCount+1
+        ridesRef.updateData([Common.mainField+".records."+String(countval):ride.toNSDictionary()]){err in
             if let err = err{
                 print("Write Error \(err)")
             }else{
-                //Increment count
-                ridesRef.updateData([Common.mainField+".count" : FieldValue.increment(Int64(1))]){err2 in
-                    if let err2 = err2{
-                        print("Error while incrementing count", err2)
+                //create chatroom
+                self.db.collection(Common.CPcollection).document("chat-1-"+String(countval)).setData([ "sentChats": FieldValue.arrayUnion([]) ], merge: true){chatroomErr in
+                    if let chatroomErr = chatroomErr{
+                        print("Error while creating chatroom",chatroomErr)
                     }else{
-                        //back to previous screen
-                        _ = self.navigationController?.popViewController(animated: true)
+                        //Increment count
+                        ridesRef.updateData([Common.mainField+".count" : FieldValue.increment(Int64(1))]){err2 in
+                            if let err2 = err2{
+                                print("Error while incrementing count", err2)
+                            }else{
+                                //back to previous screen
+                                _ = self.navigationController?.popViewController(animated: true)
+                            }
+                        }
                     }
                 }
+                
                 
             }
         }
